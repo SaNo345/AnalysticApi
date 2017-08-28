a25005b1218740f17b3fddad7c14b17d34cdf644
Sano Madoyan <madoyan1996@gmail.com>
2017-08-29 01:58:29 +0400

    Init !!!

---
+++ src/main/java/analysticapi/MyBean.java
@@ -0,0 +1,23 @@
+package analysticapi;
+
+/**
+ * Created by Sano on 27.08.2017.
+ */
+public class MyBean {
+    private String name;
+
+    public MyBean() {
+    }
+
+    public MyBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ target/analysticapi/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<head>
+    <title>Login</title>
+</head>
+    <body>
+    <s:form action="myAction">
+        <s:submit value="OK"></s:submit>
+    </s:form>
+
+    </body>
+</html>
