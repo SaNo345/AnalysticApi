1f1c9f927c5acb2ac45537207767e3f2c8017c36
Sano Madoyan <madoyan1996@gmail.com>
2017-08-30 23:20:32 +0400

    end files workers

--- src/main/java/fileworker/CSVWorker.java
+++ src/main/java/fileworker/CSVWorker.java
@@ -1,7 +1,38 @@
 package fileworker;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by Sano on 30.08.2017.
  */
 public class CSVWorker {
+    private StringBuilder csvContent;
+
+    public CSVWorker() {
+    }
+
+    // if flag is 0  content will be write in users.csv , 1 in requst.csv
+    public void write(StringBuilder content, int flag,String fileName) throws IOException {
+        if (flag == 0) {csvContent = new StringBuilder("user-id,install-date");
+        } else if (flag == 1) {csvContent = new StringBuilder("user-id,request-date");
+        } else {return;}
+        csvContent.append("\n");
+        csvContent.append(content);
+        File csv = new File(fileName);
+        if(csv.exists()){
+            csv.delete();
+        }
+
+        FileWriter writer =new FileWriter(fileName,true);
+        PrintWriter printWriter = new PrintWriter(writer,false);
+        printWriter.write(csvContent.toString());
+        printWriter.flush();
+        printWriter.close();
+        writer.close();
+
+
+    }
 }
--- src/main/java/fileworker/Test.java
+++ src/main/java/fileworker/Test.java
@@ -1,7 +1,21 @@
 package fileworker;
 
+import java.io.IOException;
+
 /**
  * Created by Sano on 30.08.2017.
  */
 public class Test {
+    public static void main(String[] args) throws IOException {
+        CSVWorker worker = new CSVWorker();
+        StringBuilder text= new StringBuilder("1;10:10:2017");
+        text.append("\n");
+        text.append("2;15:1:2017");
+
+        worker.write(text,0,"data/users.csv");
+
+        text= new StringBuilder("1;2-12-2016");
+        worker.write(text,1,"data/request.csv");
+
+    }
 }
